9d52cbc4858c75b879c95ca477e1034228615e2b
tchiotludo <tchiot.ludo@gmail.com>
2020-04-10 09:16:13 +0200

    feat(http): add a http download task

---
+++ src/test/java/org/kestra/task/fs/http/DownloadTest.java
@@ -0,0 +1,45 @@
+package org.kestra.task.fs.http;
+
+import com.google.common.collect.ImmutableMap;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.test.annotation.MicronautTest;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+import org.kestra.core.runners.RunContext;
+import org.kestra.core.storages.StorageInterface;
+import org.kestra.core.utils.TestsUtils;
+
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import javax.inject.Inject;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@MicronautTest
+class DownloadTest {
+    public static final String FILE = "https://file-examples.com/wp-content/uploads/2017/02/file_example_CSV_5000.csv";
+    @Inject
+    private ApplicationContext applicationContext;
+
+    @Inject
+    private StorageInterface storageInterface;
+
+    @Test
+    void run() throws Exception {
+        Download task = Download.builder()
+            .id(DownloadTest.class.getSimpleName())
+            .type(DownloadTest.class.getName())
+            .uri(FILE)
+            .build();
+
+        RunContext runContext = TestsUtils.mockRunContext(this.applicationContext, task, ImmutableMap.of());
+
+        Download.Output output = task.run(runContext);
+
+        assertThat(
+            IOUtils.toString(this.storageInterface.get(output.getUri()), StandardCharsets.UTF_8),
+            is(IOUtils.toString(new URL(FILE).openStream(), StandardCharsets.UTF_8))
+        );
+    }
+}
