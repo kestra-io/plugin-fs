524c20070606a3992188f65c2ce81187c783a2aa
tchiotludo <tchiot.ludo@gmail.com>
2021-03-09 11:53:21 +0100

    feat(http): add http options

--- src/main/java/org/kestra/task/fs/http/AbstractHttp.java
+++ src/main/java/org/kestra/task/fs/http/AbstractHttp.java
@@ -2,7 +2,9 @@ package org.kestra.task.fs.http;
 
 import io.micronaut.http.*;
 import io.micronaut.http.client.DefaultHttpClientConfiguration;
+import io.micronaut.http.client.HttpClientConfiguration;
 import io.micronaut.http.client.netty.DefaultHttpClient;
+import io.micronaut.logging.LogLevel;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -11,9 +13,11 @@ import org.kestra.core.models.annotations.PluginProperty;
 import org.kestra.core.models.tasks.Task;
 import org.kestra.core.runners.RunContext;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.net.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -64,10 +68,65 @@ abstract public class AbstractHttp extends Task {
     @PluginProperty(dynamic = true)
     protected Map<CharSequence, CharSequence> headers;
 
+    @Schema(
+        title = "The http request options"
+    )
+    protected RequestOptions options;
+
     protected DefaultHttpClientConfiguration configuration() {
-        // @TODO
         DefaultHttpClientConfiguration configuration = new DefaultHttpClientConfiguration();
 
+        if (this.options != null) {
+            if (this.options.getConnectTimeout() != null) {
+                configuration.setConnectTimeout(this.options.getConnectTimeout());
+            }
+
+            if (this.options.getReadTimeout() != null) {
+                configuration.setReadTimeout(this.options.getReadTimeout());
+            }
+
+            if (this.options.getReadIdleTimeout() != null) {
+                configuration.setReadIdleTimeout(this.options.getReadIdleTimeout());
+            }
+
+            if (this.options.getConnectionPoolIdleTimeout() != null) {
+                configuration.setConnectionPoolIdleTimeout(this.options.getConnectionPoolIdleTimeout());
+            }
+
+            if (this.options.getMaxContentLength() != null) {
+                configuration.setMaxContentLength(this.options.getMaxContentLength());
+            }
+
+            if (this.options.getProxyType() != null) {
+                configuration.setProxyType(this.options.getProxyType());
+            }
+
+            if (this.options.getProxyAddress() != null) {
+                configuration.setProxyAddress(this.options.getProxyAddress());
+            }
+
+            if (this.options.getProxyUsername() != null) {
+                configuration.setProxyUsername(this.options.getProxyUsername());
+            }
+
+            if (this.options.getProxyPassword() != null) {
+                configuration.setProxyPassword(this.options.getProxyPassword());
+            }
+
+            if (this.options.getDefaultCharset() != null) {
+                configuration.setDefaultCharset(this.options.getDefaultCharset());
+            }
+
+            if (this.options.getFollowRedirects() != null) {
+                configuration.setFollowRedirects(this.options.getFollowRedirects());
+            }
+
+            if (this.options.getLogLevel() != null) {
+                configuration.setLogLevel(this.options.getLogLevel());
+            }
+        }
+
+        // @TODO
         // configuration.setSslConfiguration(new SslConfiguration());
 
         return configuration;
@@ -124,5 +183,52 @@ abstract public class AbstractHttp extends Task {
         }
 
         return tags.toArray(String[]::new);
+    }
+
+    @Getter
+    @Builder
+    public static class RequestOptions {
+        @Schema(title = "The connect timeout.")
+        private final Duration connectTimeout;
+
+        @Schema(title = "The default read timeout.")
+        @Builder.Default
+        private final Duration readTimeout = Duration.ofSeconds(HttpClientConfiguration.DEFAULT_READ_TIMEOUT_SECONDS);
+
+        @Schema(title = "The default amount of time to allow read operation connections to remain idle.")
+        @Builder.Default
+        private final Duration readIdleTimeout = Duration.of(HttpClientConfiguration.DEFAULT_READ_IDLE_TIMEOUT_MINUTES, ChronoUnit.MINUTES);
+
+        @Schema(title = "The idle timeout for connection in the client connection pool. ")
+        @Builder.Default
+        private final Duration connectionPoolIdleTimeout = Duration.ofSeconds(HttpClientConfiguration.DEFAULT_CONNECTION_POOL_IDLE_TIMEOUT_SECONDS);
+
+        @Schema(title = "Sets the maximum content length the client can consume.")
+        @Builder.Default
+        private final Integer maxContentLength = HttpClientConfiguration.DEFAULT_MAX_CONTENT_LENGTH;
+
+        @Schema(title = "The proxy type to use.")
+        @Builder.Default
+        private final Proxy.Type proxyType = Proxy.Type.DIRECT;
+
+        @Schema(title = "The proxy to use.")
+        private final SocketAddress proxyAddress;
+
+        @Schema(title = "The proxy user to use.")
+        private final String proxyUsername;
+
+        @Schema(title = "The proxy password to use.")
+        private final String proxyPassword;
+
+        @Schema(title = "Sets the default charset to use.")
+        @Builder.Default
+        private final Charset defaultCharset = StandardCharsets.UTF_8;
+
+        @Schema(title = "Whether redirects should be followed.")
+        @Builder.Default
+        private final Boolean followRedirects = HttpClientConfiguration.DEFAULT_FOLLOW_REDIRECTS;
+
+        @Schema(title = "The level to enable trace logging at.")
+        private final LogLevel logLevel;
     }
 }
