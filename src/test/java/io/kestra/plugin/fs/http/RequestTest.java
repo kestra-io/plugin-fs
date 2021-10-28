29cadbbe92b199da81a50b46698c9a5159cee522
tchiotludo <tchiot.ludo@gmail.com>
2021-10-28 18:08:50 +0200

    feat(http): allow multipart upload on Request

--- src/test/java/io/kestra/plugin/fs/http/RequestTest.java
+++ src/test/java/io/kestra/plugin/fs/http/RequestTest.java
@@ -1,6 +1,9 @@
 package io.kestra.plugin.fs.http;
 
+import com.devskiller.friendly_id.FriendlyId;
+import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableMap;
+import io.kestra.core.storages.StorageInterface;
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.http.HttpMethod;
 import io.micronaut.http.HttpRequest;
@@ -8,14 +11,23 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.multipart.StreamingFileUpload;
 import io.micronaut.runtime.server.EmbeddedServer;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.reactivex.Single;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import io.kestra.core.runners.RunContext;
 import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.utils.TestsUtils;
+import org.reactivestreams.Publisher;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import javax.inject.Inject;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -24,8 +36,12 @@ import static org.hamcrest.Matchers.is;
 @MicronautTest
 class RequestTest {
     public static final String FILE = "http://www.mocky.io/v2/5ed0ce483500009300ff9f55";
+
     @Inject
     private RunContextFactory runContextFactory;
+
+    @Inject
+    private StorageInterface storageInterface;
 
     @Test
     void run() throws Exception {
@@ -97,11 +113,57 @@ class RequestTest {
         }
     }
 
+    @Test
+    void multipart() throws Exception {
+        File file = new File(Objects.requireNonNull(RequestTest.class.getClassLoader().getResource("application.yml")).toURI());
+
+        URI fileStorage = storageInterface.put(
+            new URI("/" + FriendlyId.createFriendlyId()),
+            new FileInputStream(file)
+        );
+
+        try (
+            ApplicationContext applicationContext = ApplicationContext.run();
+            EmbeddedServer server = applicationContext.getBean(EmbeddedServer.class).start();
+
+        ) {
+            Request task = Request.builder()
+                .id(RequestTest.class.getSimpleName())
+                .type(RequestTest.class.getName())
+                .method(HttpMethod.POST)
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .uri(server.getURL().toString() + "/post/multipart")
+                .formData(ImmutableMap.of("hello", "world", "file", fileStorage.toString()))
+                .build();
+
+            RunContext runContext = TestsUtils.mockRunContext(this.runContextFactory, task, ImmutableMap.of());
+
+            Request.Output output = task.run(runContext);
+
+            assertThat(output.getBody(), is("world > " + IOUtils.toString(new FileInputStream(file), Charsets.UTF_8)));
+            assertThat(output.getCode(), is(200));
+        }
+    }
+
     @Controller(value = "/post", consumes = MediaType.APPLICATION_FORM_URLENCODED)
     static class MockController {
         @Post("/simple")
         HttpResponse<String> simple(HttpRequest<?> request, String hello) {
             return HttpResponse.ok(hello + " > " + request.getHeaders().get("test"));
+        }
+
+        @Post(uri = "/multipart", consumes = MediaType.MULTIPART_FORM_DATA)
+        Single<String> multipart(HttpRequest<?> request, String hello, StreamingFileUpload file) throws IOException {
+            File tempFile = File.createTempFile(file.getFilename(), "temp");
+
+            Publisher<Boolean> uploadPublisher = file.transferTo(tempFile);
+
+            return Single.fromPublisher(uploadPublisher)
+                .map(success -> {
+                    try (FileInputStream fileInputStream = new FileInputStream(tempFile)) {
+                        return hello + " > " + IOUtils.toString(fileInputStream, StandardCharsets.UTF_8);
+                    }
+                });
         }
     }
 }
