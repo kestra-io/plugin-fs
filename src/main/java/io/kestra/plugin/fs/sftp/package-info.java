2519cc129f58295c68677af2b61fb667f77cfa1d
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:31:13 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/fs/sftp/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for accessing files using the SFTP protocol.",
+    categories = PluginSubGroup.PluginCategory.STORAGE
+)
+package io.kestra.plugin.fs.sftp;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
