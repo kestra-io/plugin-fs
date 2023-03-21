572e7d1c55bb4cebbca013854e94fb79781e7d58
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:31:13 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/fs/ftp/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for accessing files using the FTP protocol.",
+    categories = PluginSubGroup.PluginCategory.STORAGE
+)
+package io.kestra.plugin.fs.ftp;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
