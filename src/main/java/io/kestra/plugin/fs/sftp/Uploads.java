e1d064c32b2c0f4a26622586c807f6c523ea2ac3
brian-mulier-p <bmmulier@hotmail.fr>
2023-07-10 19:55:36 +0200

    feat(fs): Uploads task for mass-upload (#62)
    * feat(fs): Uploads task for mass-upload
    
    closes #61

---
+++ src/main/java/io/kestra/plugin/fs/sftp/Uploads.java
@@ -0,0 +1,60 @@
+package io.kestra.plugin.fs.sftp;
+
+import io.kestra.core.exceptions.IllegalVariableEvaluationException;
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.runners.RunContext;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.apache.commons.vfs2.FileSystemOptions;
+
+import java.io.IOException;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+        title = "Upload files to a SFTP server's directory"
+)
+@Plugin(
+        examples = {
+                @Example(
+                        code = {
+                                "host: localhost",
+                                "port: 22",
+                                "username: foo",
+                                "password: pass",
+                                "from:",
+                                "  - \"{{ outputs.taskid1.uri }}\"",
+                                "  - \"{{ outputs.taskid2.uri }}\"",
+                                "to: \"/upload/dir2\"",
+                        }
+                )
+        }
+)
+public class Uploads extends io.kestra.plugin.fs.vfs.Uploads implements SftpInterface {
+    private String keyfile;
+    private String passphrase;
+    private String proxyHost;
+    private String proxyPort;
+    private String proxyUser;
+    private String proxyPassword;
+    private String proxyType;
+    @Builder.Default
+    private Boolean rootDir = true;
+    @Builder.Default
+    private String port = "22";
+
+    @Override
+    protected FileSystemOptions fsOptions(RunContext runContext) throws IllegalVariableEvaluationException, IOException {
+        return SftpService.fsOptions(runContext, this);
+    }
+
+    @Override
+    protected String scheme() {
+        return "sftp";
+    }
+}
---
+++ src/main/java/io/kestra/plugin/fs/vfs/Uploads.java
@@ -0,0 +1,75 @@
+package io.kestra.plugin.fs.vfs;
+
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.tasks.RunnableTask;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.serializers.JacksonMapper;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.apache.commons.vfs2.impl.StandardFileSystemManager;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+
+import static io.kestra.core.utils.Rethrow.throwFunction;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+public abstract class Uploads extends AbstractVfsTask implements RunnableTask<Uploads.Output> {
+    @PluginProperty(dynamic = true)
+    @Schema(
+            title = "The files to upload, must be internal storage URIs, must be a list of URIs or a pebble template that returns a list of URIs",
+            anyOf = {
+                    String.class,
+                    String[].class
+            }
+    )
+    private Object from;
+
+    @Schema(
+            title = "The destination directory"
+    )
+    @PluginProperty(dynamic = true)
+    private String to;
+
+    public Output run(RunContext runContext) throws Exception {
+        try (StandardFileSystemManager fsm = new StandardFileSystemManager()) {
+            fsm.init();
+
+            String[] renderedFrom;
+            if (this.from instanceof String[] fromURIs) {
+                renderedFrom = fromURIs;
+            } else {
+                renderedFrom = JacksonMapper.ofJson().readValue(runContext.render((String) this.from), String[].class);
+            }
+            List<Upload.Output> outputs = Arrays.stream(renderedFrom).map(throwFunction((fromURI) -> VfsService.upload(
+                    runContext,
+                    fsm,
+                    this.fsOptions(runContext),
+                    URI.create(runContext.render(fromURI)),
+                    this.uri(runContext, this.to + fromURI.substring(fromURI.lastIndexOf('/') + (this.to.endsWith("/") ? 1 : 0)))
+            ))).toList();
+
+            return Output.builder()
+                    .files(outputs.stream()
+                            .map(Upload.Output::getTo)
+                            .toList()
+                    )
+                    .build();
+        }
+    }
+
+    @Builder
+    @Getter
+    public static class Output implements io.kestra.core.models.tasks.Output {
+        @Schema(
+                title = "The fully-qualified URIs that point to the uploaded files on remote"
+        )
+        private List<URI> files;
+    }
+}
