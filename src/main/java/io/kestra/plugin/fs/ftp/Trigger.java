af27ae97c5d07337b9ae86860b4704c458d1713a
Anna Geller <anna.m.geller@gmail.com>
2023-11-02 13:19:13 +0100

    feat(docs): extend the docs with more examples and explanation of properties (#77)
    closes #75

--- src/main/java/io/kestra/plugin/fs/ftp/Trigger.java
+++ src/main/java/io/kestra/plugin/fs/ftp/Trigger.java
@@ -18,25 +18,25 @@ import java.net.Proxy;
 @Getter
 @NoArgsConstructor
 @Schema(
-    title = "Wait for files on a FTP server"
+    title = "Trigger a flow as soon as new files are detected in a given FTP server's directory."
 )
 @Plugin(
     examples = {
         @Example(
-            title = "Wait for a list of file on a FTP server and iterate through the files",
+            title = "Wait for one or more files in a given FTP server's directory and process each of these files sequentially.",
             full = true,
             code = {
-                "id: gcs-listen",
-                "namespace: io.kestra.tests",
+                "id: ftp_trigger_flow",
+                "namespace: dev",
                 "",
                 "tasks:",
-                "  - id: each",
+                "  - id: for_each_file",
                 "    type: io.kestra.core.tasks.flows.EachSequential",
+                "    value: \"{{ trigger.files | jq('.path') }}\"",
                 "    tasks:",
                 "      - id: return",
                 "        type: io.kestra.core.tasks.debugs.Return",
-                "        format: \"{{taskrun.value}}\"",
-                "    value: \"{{ trigger.files | jq('.path') }}\"",
+                "        format: \"{{ taskrun.value }}\"",
                 "",
                 "triggers:",
                 "  - id: watch",
@@ -44,13 +44,42 @@ import java.net.Proxy;
                 "    host: localhost",
                 "    port: 21",
                 "    username: foo",
-                "    password: pass",
+                "    password: bar",
                 "    from: \"/in/\"",
                 "    interval: PT10S",
                 "    action: MOVE",
                 "    moveDirectory: \"/archive/\"",
             }
-        )
+        ),
+        @Example(
+            title = "Wait for one or more files in a given FTP server's directory and process each of these files sequentially. In this example, we restrict the trigger to only wait for CSV files in the `mydir` directory.",
+            full = true,
+            code = """
+                id: ftp_wait_for_csv_in_mydir
+                namespace: dev
+
+                tasks:
+                  - id: each
+                    type: io.kestra.core.tasks.flows.EachSequential
+                    value: "{{ trigger.files | jq('.path') }}"
+                    tasks:
+                      - id: return
+                        type: io.kestra.core.tasks.debugs.Return
+                        format: "{{ taskrun.value }}"
+                    
+                triggers:
+                  - id: watch
+                    type: io.kestra.plugin.fs.ftp.Trigger
+                    host: localhost
+                    port: "21"
+                    username: foo
+                    password: bar
+                    from: "mydir/"
+                    regExp: ".*.csv"
+                    action: MOVE
+                    moveDirectory: "archive/"
+                    interval: PTS"""
+        )        
     }
 )
 public class Trigger extends io.kestra.plugin.fs.vfs.Trigger implements FtpInterface {
--- src/main/java/io/kestra/plugin/fs/sftp/Trigger.java
+++ src/main/java/io/kestra/plugin/fs/sftp/Trigger.java
@@ -17,25 +17,25 @@ import java.io.IOException;
 @Getter
 @NoArgsConstructor
 @Schema(
-    title = "Wait for files on a SFTP server"
+    title = "Trigger a flow as soon as new files are detected in a given SFTP server's directory."
 )
 @Plugin(
     examples = {
         @Example(
-            title = "Wait for a list of file on a SFTP server and iterate through the files",
+            title = "Wait for one or more files in a given SFTP server's directory and process each of these files sequentially.",
             full = true,
             code = {
-                "id: gcs-listen",
-                "namespace: io.kestra.tests",
+                "id: sftp_trigger_flow",
+                "namespace: dev",
                 "",
                 "tasks:",
-                "  - id: each",
+                "  - id: for_each_file",
                 "    type: io.kestra.core.tasks.flows.EachSequential",
+                "    value: \"{{ trigger.files | jq('.path') }}\"",
                 "    tasks:",
                 "      - id: return",
                 "        type: io.kestra.core.tasks.debugs.Return",
-                "        format: \"{{taskrun.value}}\"",
-                "    value: \"{{ trigger.files | jq('.path') }}\"",
+                "        format: \"{{ taskrun.value }}\"",
                 "",
                 "triggers:",
                 "  - id: watch",
@@ -43,13 +43,42 @@ import java.io.IOException;
                 "    host: localhost",
                 "    port: 6622",
                 "    username: foo",
-                "    password: pass",
+                "    password: bar",
                 "    from: \"/in/\"",
                 "    interval: PT10S",
                 "    action: MOVE",
                 "    moveDirectory: \"/archive/\"",
             }
-        )
+        ),
+        @Example(
+            title = "Wait for one or more files in a given SFTP server's directory and process each of these files sequentially. In this example, we restrict the trigger to only wait for CSV files in the `mydir` directory.",
+            full = true,
+            code = """
+                id: ftp_wait_for_csv_in_mydir
+                namespace: dev
+
+                tasks:
+                  - id: each
+                    type: io.kestra.core.tasks.flows.EachSequential
+                    value: "{{ trigger.files | jq('.path') }}"
+                    tasks:
+                      - id: return
+                        type: io.kestra.core.tasks.debugs.Return
+                        format: "{{ taskrun.value }}"
+                    
+                triggers:
+                  - id: watch
+                    type: io.kestra.plugin.fs.sftp.Trigger
+                    host: localhost
+                    port: "6622"
+                    username: foo
+                    password: bar
+                    from: "mydir/"
+                    regExp: ".*.csv"
+                    action: MOVE
+                    moveDirectory: "archive/"
+                    interval: PTS"""
+        )          
     }
 )
 public class Trigger extends io.kestra.plugin.fs.vfs.Trigger implements SftpInterface {
