c74c84fb7eed9dc912af4e92368e86db9f76087f
Anna Geller <anna.m.geller@gmail.com>
2023-11-02 13:19:13 +0100

    feat(docs): extend the docs with more examples and explanation of properties (#77)
    closes #75

--- src/main/java/io/kestra/plugin/fs/ftp/FtpInterface.java
+++ src/main/java/io/kestra/plugin/fs/ftp/FtpInterface.java
@@ -31,7 +31,7 @@ public interface FtpInterface {
     Boolean getRootDir();
 
     @Schema(
-        title = "Enter into passive mode."
+        title = "Whether to use a [passive mode](https://www.jscape.com/blog/active-v-s-passive-ftp-simplified). Passive mode is generally considered more secure as it's less likely to encounter issues with NAT and firewalls. Therefore, this property is by default set to `true`. To use active mode instead, set the property to `false`."
     )
     @PluginProperty(dynamic = false)
     Boolean getPassiveMode();
--- src/main/java/io/kestra/plugin/fs/ftps/Trigger.java
+++ src/main/java/io/kestra/plugin/fs/ftps/Trigger.java
@@ -21,39 +21,68 @@ import java.net.Proxy;
 @Getter
 @NoArgsConstructor
 @Schema(
-    title = "Wait for files on a FTPS server"
+    title = "Trigger a flow as soon as new files are detected in a given FTPS server's directory."
 )
 @Plugin(
     examples = {
         @Example(
-            title = "Wait for a list of file on a FTP server and iterate through the files",
+            title = "Wait for one or more files in a given FTPS server's directory and process each of these files sequentially.",
             full = true,
             code = {
-                "id: gcs-listen",
-                "namespace: io.kestra.tests",
+                "id: ftps_trigger_flow",
+                "namespace: dev",
                 "",
                 "tasks:",
-                "  - id: each",
+                "  - id: for_each_file",
                 "    type: io.kestra.core.tasks.flows.EachSequential",
+                "    value: \"{{ trigger.files | jq('.path') }}\"",
                 "    tasks:",
                 "      - id: return",
                 "        type: io.kestra.core.tasks.debugs.Return",
-                "        format: \"{{taskrun.value}}\"",
-                "    value: \"{{ trigger.files | jq('.path') }}\"",
+                "        format: \"{{ taskrun.value }}\"",
                 "",
                 "triggers:",
                 "  - id: watch",
-                "    type: io.kestra.plugin.fs.ftp.Trigger",
+                "    type: io.kestra.plugin.fs.ftps.Trigger",
                 "    host: localhost",
                 "    port: 990",
                 "    username: foo",
-                "    password: pass",
+                "    password: bar",
                 "    from: \"/in/\"",
                 "    interval: PT10S",
                 "    action: MOVE",
                 "    moveDirectory: \"/archive/\"",
             }
-        )
+        ),
+        @Example(
+            title = "Wait for one or more files in a given FTPS server's directory and process each of these files sequentially. In this example, we restrict the trigger to only wait for CSV files in the `mydir` directory.",
+            full = true,
+            code = """
+                id: ftp_wait_for_csv_in_mydir
+                namespace: dev
+
+                tasks:
+                  - id: each
+                    type: io.kestra.core.tasks.flows.EachSequential
+                    value: "{{ trigger.files | jq('.path') }}"
+                    tasks:
+                      - id: return
+                        type: io.kestra.core.tasks.debugs.Return
+                        format: "{{ taskrun.value }}"
+                    
+                triggers:
+                  - id: watch
+                    type: io.kestra.plugin.fs.ftps.Trigger
+                    host: localhost
+                    port: "21"
+                    username: foo
+                    password: bar
+                    from: "mydir/"
+                    regExp: ".*.csv"
+                    action: MOVE
+                    moveDirectory: "archive/"
+                    interval: PTS"""
+        ) 
     }
 )
 public class Trigger extends io.kestra.plugin.fs.vfs.Trigger implements FtpInterface, FtpsInterface {
