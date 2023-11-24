ca3dd782d8772176f881f2b28c485670296f1f60
Loïc Mathieu <loikeseke@gmail.com>
2023-11-24 16:50:21 +0100

    feat: HTTP Trigger

---
+++ src/main/java/io/kestra/plugin/fs/http/Trigger.java
@@ -0,0 +1,145 @@
+package io.kestra.plugin.fs.http;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.conditions.ConditionContext;
+import io.kestra.core.models.executions.Execution;
+import io.kestra.core.models.executions.ExecutionTrigger;
+import io.kestra.core.models.flows.State;
+import io.kestra.core.models.triggers.AbstractTrigger;
+import io.kestra.core.models.triggers.PollingTriggerInterface;
+import io.kestra.core.models.triggers.TriggerContext;
+import io.kestra.core.models.triggers.TriggerOutput;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.utils.TruthUtils;
+import io.micronaut.http.HttpMethod;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.slf4j.Logger;
+
+import javax.validation.constraints.NotNull;
+import java.time.Duration;
+import java.util.Map;
+import java.util.Optional;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+        title = "Trigger a flow based on an HTTP response"
+)
+@Plugin(
+        examples = {
+                @Example(
+                        title = "Trigger a flow if an HTTP endpoint returns a status code equals to 200",
+                        full = true,
+                        code = {
+                                """
+                                id: http-trigger
+                                namespace: dev
+                                                        
+                                triggers:
+                                  - id: http
+                                    type: io.kestra.plugin.fs.http.Trigger
+                                    uri: https://api.chucknorris.io/jokes/random
+                                    responseCondition: "{{ response.statusCode == 200 }}"
+                                                        
+                                tasks:
+                                  - id: hello
+                                    type: io.kestra.core.tasks.log.Log
+                                    message: Kestra team wishes you a great day! 👋
+                                """
+                        }
+                )
+        }
+)
+public class Trigger extends AbstractTrigger implements PollingTriggerInterface, HttpInterface, TriggerOutput<Request.Output> {
+    @Builder.Default
+    private final Duration interval = Duration.ofSeconds(60);
+
+    @Schema(
+            title = "The condition on the HTTP response to trigger a flow which can be any expression that evaluates to a boolean value.",
+            description = """
+                The condition will be evaluated after calling the HTTP endpoint, it can use the response itself to determine whether to start a flow or not.
+                The following variables are available when evaluating the condition:
+                - `response.statusCode`: the response HTTP status code
+                - `response.body`: the response body as a string
+                - `response.headers`: the response headers
+                
+                Boolean coercion allows 0, -0, null and '' to evaluate to false, all other values will evaluate to true.
+                
+                The condition will be evaluated before any 'generic trigger conditions' that can be configured via the `conditions` property.
+                """
+    )
+    @PluginProperty(dynamic = true)
+    @Builder.Default
+    @NotNull
+    private String responseCondition = "{{ response.statusCode < 400 }}";
+
+    @NotNull
+    private String uri;
+
+    @Builder.Default
+    private HttpMethod method = HttpMethod.GET;
+
+    private String body;
+
+    private Map<String, Object> formData;
+
+    private String contentType;
+
+    private Map<CharSequence, CharSequence> headers;
+
+    private RequestOptions options;
+
+    private SslOptions sslOptions;
+
+
+    @Override
+    public Optional<Execution> evaluate(ConditionContext conditionContext, TriggerContext context) throws Exception {
+        RunContext runContext = conditionContext.getRunContext();
+        Logger logger = runContext.logger();
+
+        var request = Request.builder()
+                .uri(this.uri)
+                .method(this.method)
+                .body(this.body)
+                .formData(this.formData)
+                .contentType(this.contentType)
+                .headers(this.headers)
+                .options(this.options)
+                .sslOptions(this.sslOptions)
+                // we allow failed status code as it is the condition that must determine whether we trigger the flow
+                .allowFailed(true)
+                .build();
+        var output = request.run(runContext);
+
+        logger.debug("{} respond with status code '{}'", output.getUri(), output.getCode());
+
+        Map<String, Object> responseVariables = Map.of("response", Map.of(
+            "statusCode", output.getCode(),
+            "body", output.getBody(),
+            "headers", output.getHeaders()
+            )
+        );
+        var renderedCondition = runContext.render(this.responseCondition, responseVariables);
+        if (TruthUtils.isTruthy(renderedCondition)) {
+            var executionTrigger = ExecutionTrigger.of(this, output);
+            var execution = Execution.builder()
+                    .id(runContext.getTriggerExecutionId())
+                    .namespace(context.getNamespace())
+                    .flowId(context.getFlowId())
+                    .flowRevision(context.getFlowRevision())
+                    .state(new State())
+                    .trigger(executionTrigger)
+                    .build();
+            return Optional.of(execution);
+        }
+
+        return Optional.empty();
+    }
+}
