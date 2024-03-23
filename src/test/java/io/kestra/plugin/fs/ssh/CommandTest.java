1b57cc6e1eb6307deb11bf0e5558ceb46290a466
Alexandre Burgoni <burgoni.alexandre@protonmail.com>
2024-03-23 11:51:37 +0100

    feat(ssh): allow to use pubkey auth (#111)
    * feat(ssh): allow to use pubkey auth
    
    Signed-off-by: AlexandreBrg <alexandre.burgoni@clever-cloud.com>
    
    * fix(ssh): test key generation
    
    Signed-off-by: AlexandreBrg <alexandre.burgoni@clever-cloud.com>
    
    * tests(ssh): use dynamically generated keys
    
    Signed-off-by: AlexandreBrg <alexandre.burgoni@clever-cloud.com>
    
    ---------
    
    Signed-off-by: AlexandreBrg <alexandre.burgoni@clever-cloud.com>

--- src/test/java/io/kestra/plugin/fs/ssh/CommandTest.java
+++ src/test/java/io/kestra/plugin/fs/ssh/CommandTest.java
@@ -7,13 +7,25 @@ import io.kestra.core.queues.QueueFactoryInterface;
 import io.kestra.core.queues.QueueInterface;
 import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.utils.TestsUtils;
+import io.kestra.plugin.fs.ssh.SshInterface.AuthMethod;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import java.nio.charset.*;
+
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPrivateKeySpec;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -31,7 +43,7 @@ class CommandTest {
     private QueueInterface<LogEntry> logQueue;
 
     @Test
-    void run() throws Exception {
+    void run_passwordMethod() throws Exception {
         List<LogEntry> logs = new ArrayList<>();
         logQueue.receive(l -> logs.add(l.getLeft()));
 
@@ -40,6 +52,7 @@ class CommandTest {
             .type(CommandTest.class.getName())
             .host("localhost")
             .username("foo")
+            .authMethod(AuthMethod.PASSWORD)
             .password("password")
             .port("2222")
             .commands(new String[] {
@@ -60,5 +73,47 @@ class CommandTest {
         assertThat(run.getStdErrLineCount(), is(2));
         assertThat(run.getVars().get("out"), is("1"));
         assertThat(run.getVars().get("err"), is("2"));
+    }
+
+    @Test
+    void run_pubkeyMethod() throws Exception {
+        List<LogEntry> logs = new ArrayList<>();
+        logQueue.receive(l -> logs.add(l.getLeft()));
+
+        File file = new File("src/test/resources/ssh/id_ed25519");
+        byte[] data;
+        try (FileInputStream fis = new FileInputStream(file)) {
+            data = new byte[(int) file.length()];
+            fis.read(data);
+        }
+        String keyFileContent = new String(data, StandardCharsets.UTF_8);
+
+        Command command = Command.builder()
+            .id(CommandTest.class.getName())
+            .type(CommandTest.class.getName())
+            .host("localhost")
+            .username("foo")
+            .authMethod(AuthMethod.PUBLIC_KEY)
+            .privateKey(keyFileContent)
+            .port("2222")
+            .commands(new String[] {
+                "echo 0",
+                "echo 1",
+                ">&2 echo 2",
+                "echo '::{\"outputs\":{\"out\":\"1\"}}::'",
+                ">&2 echo '::{\"outputs\":{\"err\":\"2\"}}::'",
+            })
+            .build();
+
+        Command.ScriptOutput run = command.run(TestsUtils.mockRunContext(runContextFactory, command, ImmutableMap.of()));
+
+        Thread.sleep(500);
+
+        assertThat(run.getExitCode(), is(0));
+        assertThat(run.getStdOutLineCount(), is(3));
+        assertThat(run.getStdErrLineCount(), is(2));
+        assertThat(run.getVars().get("out"), is("1"));
+        assertThat(run.getVars().get("err"), is("2"));
+
     }
 }
