42ddfc7b62df04703c26caacfc067f28ee752d99
Loïc Mathieu <loikeseke@gmail.com>
2024-05-16 15:10:31 +0200

    feat: move HTTP tasks to core

--- src/main/java/io/kestra/plugin/fs/http/package-info.java
+++
@@ -1,7 +0,0 @@
-@PluginSubGroup(
-    description = "This sub-group of plugins contains tasks for making HTTP requests.",
-    categories = PluginSubGroup.PluginCategory.STORAGE
-)
-package io.kestra.plugin.fs.http;
-
-import io.kestra.core.models.annotations.PluginSubGroup;
