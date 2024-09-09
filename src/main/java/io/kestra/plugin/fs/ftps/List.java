b3256bfcb4f0dc405c81e014b0063fcdb820b493
Shruti Mantri <smantri@moveworks.ai>
2024-09-09 14:47:16 +0530

    fix(docs): correct the regex in the example

--- src/main/java/io/kestra/plugin/fs/ftps/List.java
+++ src/main/java/io/kestra/plugin/fs/ftps/List.java
@@ -39,7 +39,7 @@ import java.net.Proxy;
                     username: foo
                     password: pass
                     from: "/upload/dir1/"
-                    regExp: ".*\/dir1\/.*\.(yaml|yml)"
+                    regExp: ".*\\/dir1\\/.*.(yaml|yml)"
                 """
         )
     }
