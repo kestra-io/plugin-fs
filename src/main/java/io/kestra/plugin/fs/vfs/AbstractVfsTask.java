a9cb37d34ce47a6b209ffe0f36220697794475b5
François Delbrayelle <fdelbrayelle@gmail.com>
2025-07-10 14:31:46 +0200

    feat(security): add RSA/SHA1 option (#207)

--- src/main/java/io/kestra/plugin/fs/vfs/AbstractVfsTask.java
+++ src/main/java/io/kestra/plugin/fs/vfs/AbstractVfsTask.java
@@ -1,13 +1,14 @@
 package io.kestra.plugin.fs.vfs;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
 import io.kestra.core.exceptions.IllegalVariableEvaluationException;
 import io.kestra.core.models.property.Property;
 import io.kestra.core.models.tasks.Task;
 import io.kestra.core.runners.RunContext;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.apache.commons.vfs2.FileSystemOptions;
 
@@ -25,11 +26,35 @@ public abstract class AbstractVfsTask extends Task implements AbstractVfsInterfa
     protected Property<String> username;
     protected Property<String> password;
 
+    @Builder.Default
+    @Schema(
+        title = "Enable the disabled by default RSA/SHA1 algorithm"
+    )
+    @NotNull
+    private Property<Boolean> enableSshRsa1 = Property.ofValue(false);
+
     protected abstract FileSystemOptions fsOptions(RunContext runContext) throws IllegalVariableEvaluationException, IOException;
+
     protected abstract String scheme();
 
-    protected URI uri(RunContext runContext, String filepath) throws IllegalVariableEvaluationException, URISyntaxException {
-        return VfsService.uri(runContext, 
+    protected URI uri(RunContext runContext, String filepath) throws IllegalVariableEvaluationException, URISyntaxException, JSchException {
+
+        var renderedHost = runContext.render(this.host).as(String.class).orElseThrow();
+        var renderedPort = runContext.render(this.getPort()).as(String.class).orElseThrow();
+        var jsch = new JSch();
+        var session = jsch.getSession(
+            runContext.render(username).as(String.class).orElse(null),
+            renderedHost, Integer.parseInt(renderedPort)
+        );
+
+        // enable disabled by default weak RSA/SHA1 algorithm
+        if (runContext.render(enableSshRsa1).as(Boolean.class).orElseThrow()) {
+            runContext.logger().info("RSA/SHA1 is enabled, be advise that SHA1 is no longer considered secure by the general cryptographic community.");
+            session.setConfig("server_host_key", session.getConfig("server_host_key") + ",ssh-rsa");
+            session.setConfig("PubkeyAcceptedAlgorithms", session.getConfig("PubkeyAcceptedAlgorithms") + ",ssh-rsa");
+        }
+
+        return VfsService.uri(runContext,
             this.scheme(),
             runContext.render(this.host).as(String.class).orElse(null),
             runContext.render(this.getPort()).as(String.class).orElse(null),
