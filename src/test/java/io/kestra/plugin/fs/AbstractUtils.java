08673f97347abc44ce06a1186e1eb6115a9dd641
Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>
2025-10-20 15:23:22 +0530

    feat(triggers): introduce stateful trigger (#236)

--- src/test/java/io/kestra/plugin/fs/AbstractUtils.java
+++ src/test/java/io/kestra/plugin/fs/AbstractUtils.java
@@ -4,6 +4,7 @@ import com.devskiller.friendly_id.FriendlyId;
 import io.kestra.core.models.property.Property;
 import io.kestra.core.storages.StorageInterface;
 import io.kestra.core.tenant.TenantService;
+import io.kestra.core.utils.IdUtils;
 import io.kestra.plugin.fs.sftp.Upload;
 import io.kestra.plugin.fs.vfs.Delete;
 import io.kestra.plugin.fs.vfs.List;
@@ -11,7 +12,10 @@ import jakarta.inject.Inject;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.Objects;
 
 public abstract class AbstractUtils {
@@ -35,6 +39,26 @@ public abstract class AbstractUtils {
 
     public Upload.Output upload(String to) throws Exception {
         return this.upload(uploadToStorage(), to);
+    }
+
+    public Upload.Output update(String to) throws Exception {
+        return this.update(to, "Updated content -" + IdUtils.create());
+    }
+
+    public Upload.Output update(String to, String content) throws Exception {
+        File tempFile = File.createTempFile("update-", ".yml");
+        try (FileWriter writer = new FileWriter(tempFile, StandardCharsets.UTF_8)) {
+            writer.write(content);
+        }
+
+        URI storageUri = storageInterface.put(
+            TenantService.MAIN_TENANT,
+            null,
+            new URI("/" + FriendlyId.createFriendlyId()),
+            Files.newInputStream(tempFile.toPath())
+        );
+
+        return this.upload(storageUri, to);
     }
 
     abstract public Upload.Output upload(URI source, String to) throws Exception;
--- src/test/java/io/kestra/plugin/fs/sftp/TriggerTest.java
+++ src/test/java/io/kestra/plugin/fs/sftp/TriggerTest.java
@@ -3,6 +3,8 @@ package io.kestra.plugin.fs.sftp;
 import com.devskiller.friendly_id.FriendlyId;
 import io.kestra.core.models.executions.Execution;
 import io.kestra.core.models.property.Property;
+import io.kestra.core.models.triggers.StatefulTriggerInterface;
+import io.kestra.core.utils.IdUtils;
 import io.kestra.core.utils.TestsUtils;
 import io.kestra.plugin.fs.AbstractFileTriggerTest;
 import io.kestra.plugin.fs.AbstractUtils;
@@ -12,6 +14,7 @@ import jakarta.inject.Inject;
 import org.apache.commons.vfs2.FileSystemException;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.util.Map;
 import java.util.Optional;
 
@@ -88,5 +91,84 @@ public class TriggerTest extends AbstractFileTriggerTest {
 
         io.kestra.plugin.fs.vfs.Download.Output run = task.run(TestsUtils.mockRunContext(runContextFactory, task, Map.of()));
         assertThat(run.getTo().toString(), containsString("kestra://"));
+    }
+
+    @Test
+    void shouldTriggerOnCreate() throws Exception {
+        var trigger = Trigger.builder()
+            .id("sftp-create")
+            .type(Trigger.class.getName())
+            .host(Property.ofValue("localhost"))
+            .port(Property.ofValue("6622"))
+            .username(USERNAME)
+            .password(PASSWORD)
+            .action(Property.ofValue(Downloads.Action.NONE))
+            .from(Property.ofValue("/upload/trigger/on-create/"))
+            .on(Property.ofValue(StatefulTriggerInterface.On.CREATE))
+            .interval(Duration.ofSeconds(5))
+            .build();
+
+        String file = FriendlyId.createFriendlyId();
+        utils().upload("/upload/trigger/on-create/" + file + ".yml");
+
+        var context = TestsUtils.mockTrigger(runContextFactory, trigger);
+        Optional<Execution> execution = trigger.evaluate(context.getKey(), context.getValue());
+        assertThat(execution.isPresent(), is(true));
+    }
+
+    @Test
+    void shouldTriggerOnUpdate() throws Exception {
+        String file = FriendlyId.createFriendlyId();
+        utils().upload("/upload/trigger/on-update/" + file + ".yml");
+
+        var trigger = Trigger.builder()
+            .id("sftp-update" + IdUtils.create())
+            .type(Trigger.class.getName())
+            .host(Property.ofValue("localhost"))
+            .port(Property.ofValue("6622"))
+            .username(USERNAME)
+            .password(PASSWORD)
+            .action(Property.ofValue(Downloads.Action.NONE))
+            .from(Property.ofValue("/upload/trigger/on-update/"))
+            .on(Property.ofValue(StatefulTriggerInterface.On.UPDATE))
+            .interval(Duration.ofSeconds(5))
+            .build();
+
+        var context = TestsUtils.mockTrigger(runContextFactory, trigger);
+        trigger.evaluate(context.getKey(), context.getValue());
+
+        utils().update("/upload/trigger/on-update/" + file + ".yml");
+
+        Optional<Execution> execution = trigger.evaluate(context.getKey(), context.getValue());
+        assertThat(execution.isPresent(), is(true));
+    }
+
+    @Test
+    void shouldTriggerOnCreateOrUpdate() throws Exception {
+        String file = FriendlyId.createFriendlyId();
+
+        var trigger = Trigger.builder()
+            .id("sftp-create-or-update" + IdUtils.create())
+            .type(Trigger.class.getName())
+            .host(Property.ofValue("localhost"))
+            .port(Property.ofValue("6622"))
+            .username(USERNAME)
+            .password(PASSWORD)
+            .action(Property.ofValue(Downloads.Action.NONE))
+            .from(Property.ofValue("/upload/trigger/on-create-or-update/"))
+            .on(Property.ofValue(StatefulTriggerInterface.On.CREATE_OR_UPDATE))
+            .interval(Duration.ofSeconds(5))
+            .build();
+
+        utils().upload("/upload/trigger/on-create-or-update/" + file + ".yml");
+
+        var context = TestsUtils.mockTrigger(runContextFactory, trigger);
+        Optional<Execution> first = trigger.evaluate(context.getKey(), context.getValue());
+        assertThat(first.isPresent(), is(true));
+
+        utils().upload("/upload/trigger/on-create-or-update/" + file + ".txt");
+
+        Optional<Execution> second = trigger.evaluate(context.getKey(), context.getValue());
+        assertThat(second.isPresent(), is(true));
     }
 }
