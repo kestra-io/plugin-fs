48dfbeadb1c4a54040603bdd50627df062fb0007
Sourav Agrawal <souravagrawal.1023@gmail.com>
2025-10-21 12:35:41 +0530

    fix: Made setting password conditional if present (#238)
    * fix: Made setting password conditional if present
    
    * refacotr:Renamed renderedPassword to rPassword

--- src/main/java/io/kestra/plugin/fs/ssh/Command.java
+++ src/main/java/io/kestra/plugin/fs/ssh/Command.java
@@ -219,10 +219,11 @@ public class Command extends Task implements SshInterface, RunnableTask<ScriptOu
                 session.setConfig("PubkeyAcceptedAlgorithms", session.getConfig("PubkeyAcceptedAlgorithms") + ",ssh-rsa");
             }
 
+            var rPassword = runContext.render(this.password).as(String.class);
             switch (renderedAuthMethod) {
                 case PASSWORD:
                     session.setConfig("PreferredAuthentications", "password");
-                    session.setPassword(runContext.render(this.password).as(String.class).orElseThrow());
+                    session.setPassword(rPassword.orElseThrow());
                     break;
                 case PUBLIC_KEY:
                     session.setConfig("PreferredAuthentications", "publickey");
@@ -240,7 +241,7 @@ public class Command extends Task implements SshInterface, RunnableTask<ScriptOu
                     }
                     ConfigRepository configRepository = OpenSSHConfig.parseFile(configPath);
                     jsch.setConfigRepository(configRepository);
-                    session.setPassword(runContext.render(password).as(String.class).orElseThrow());
+                    rPassword.ifPresent(session::setPassword);
                     break;
             }
 
