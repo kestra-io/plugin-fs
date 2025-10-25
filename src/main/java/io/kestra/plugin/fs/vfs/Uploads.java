1d7b9a952e1acc8b1f82b714a6cd158f5e5a087e
Sahil Lather <sahillather002@gmail.com>
2025-10-25 13:50:09 +0000

    refactor(): use ubiquitous Data.From
    Removed unused Map import and updated the 'from' field description in the Uploads class. Refactored the output reading logic to handle URI strings more effectively.

--- src/main/java/io/kestra/plugin/fs/vfs/Uploads.java
+++ src/main/java/io/kestra/plugin/fs/vfs/Uploads.java
@@ -1,10 +1,10 @@
 package io.kestra.plugin.fs.vfs;
 
 import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.property.Data;
 import io.kestra.core.models.property.Property;
 import io.kestra.core.models.tasks.RunnableTask;
 import io.kestra.core.runners.RunContext;
-import io.kestra.core.serializers.JacksonMapper;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -12,7 +12,6 @@ import lombok.experimental.SuperBuilder;
 import org.apache.commons.vfs2.impl.StandardFileSystemManager;
 
 import java.net.URI;
-import java.util.Arrays;
 import java.util.List;
 
 import static io.kestra.core.utils.Rethrow.throwFunction;
@@ -23,14 +22,15 @@ import static io.kestra.core.utils.Rethrow.throwFunction;
 @Getter
 @NoArgsConstructor
 public abstract class Uploads extends AbstractVfsTask implements RunnableTask<Uploads.Output> {
-    @PluginProperty(dynamic = true, internalStorageURI = true)
     @Schema(
-            title = "The files to upload, must be internal storage URIs, must be a list of URIs or a pebble template that returns a list of URIs",
-            anyOf = {
-                    String.class,
-                    String[].class
-            }
+        title = "The files to upload, must be internal storage URIs, must be a list of URIs or a pebble template that returns a list of URIs",
+        anyOf = {
+            String.class,
+            String[].class
+        },
+        description = "Must be Kestra internal storage URIs. Can be a single URI string, a list of URI strings, or an internal storage URI pointing to a file containing URIs."
     )
+    @PluginProperty(dynamic = true, internalStorageURI = true)
     @NotNull
     private Object from;
 
@@ -45,26 +45,25 @@ public abstract class Uploads extends AbstractVfsTask implements RunnableTask<Up
             fsm.setConfiguration(StandardFileSystemManager.class.getResource(KestraStandardFileSystemManager.CONFIG_RESOURCE));
             fsm.init();
 
-            String[] renderedFrom;
-            if (this.from instanceof List<?> fromURIs) {
-                renderedFrom = fromURIs.stream().map(throwFunction(from -> runContext.render((String) from))).toArray(String[]::new);
-            } else {
-                renderedFrom = JacksonMapper.ofJson().readValue(runContext.render((String) this.from), String[].class);
-            }
-            List<Upload.Output> outputs = Arrays.stream(renderedFrom).map(throwFunction(fromURI -> {
-                if (!fromURI.startsWith("kestra://")) {
-                    throw new IllegalArgumentException("'from' must be a list of Kestra's internal storage URI");
-                }
-                String renderedTo = runContext.render(this.to).as(String.class).orElseThrow();
-                return VfsService.upload(
-                    runContext,
-                    fsm,
-                    this.fsOptions(runContext),
-                    URI.create(fromURI),
-                    this.uri(runContext, renderedTo + fromURI.substring(fromURI.lastIndexOf('/') + (renderedTo.endsWith("/") ? 1 : 0)))
-                );
-            }
-            )).toList();
+            String renderedTo = runContext.render(this.to).as(String.class).orElseThrow();
+
+            List<Upload.Output> outputs = Data.from(from)
+                .readAs(runContext, String.class, obj -> obj.toString())
+                .map(throwFunction(fromURI -> {
+                    if (!fromURI.startsWith("kestra://")) {
+                        throw new IllegalArgumentException("'from' must be a list of Kestra's internal storage URI");
+                    }
+
+                    return VfsService.upload(
+                        runContext,
+                        fsm,
+                        this.fsOptions(runContext),
+                        URI.create(fromURI),
+                        this.uri(runContext, renderedTo + fromURI.substring(fromURI.lastIndexOf('/') + (renderedTo.endsWith("/") ? 1 : 0)))
+                    );
+                }))
+                .collectList()
+                .block();
 
             return Output.builder()
                     .files(outputs.stream()
