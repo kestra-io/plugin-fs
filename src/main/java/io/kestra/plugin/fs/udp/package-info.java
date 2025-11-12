6858ad7f9f2764e4e9bce950e587fa99577dd20b
Divanshu Prajapat <78331299+idivanshu@users.noreply.github.com>
2025-11-12 21:04:30 +0530

    chore(fs-udp): update plugin category from STORAGE to MESSAGING
    Co-authored-by: François Delbrayelle <fdelbrayelle@gmail.com>

--- src/main/java/io/kestra/plugin/fs/udp/package-info.java
+++ src/main/java/io/kestra/plugin/fs/udp/package-info.java
@@ -1,6 +1,6 @@
 @io.kestra.core.models.annotations.PluginSubGroup(
     title = "UDP",
     description = "This sub-group of plugins provides tasks and triggers for interacting with UDP socket connections — including sending and receiving data over UDP.",
-    categories = io.kestra.core.models.annotations.PluginSubGroup.PluginCategory.STORAGE
+    categories = io.kestra.core.models.annotations.PluginSubGroup.PluginCategory.MESSAGING
 )
 package io.kestra.plugin.fs.udp;
