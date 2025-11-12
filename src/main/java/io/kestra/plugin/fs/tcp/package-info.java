896a1de7cb98701dd6c5fc6443b84a6bbfae8537
Divanshu Prajapat <78331299+idivanshu@users.noreply.github.com>
2025-11-12 21:04:47 +0530

    Update src/main/java/io/kestra/plugin/fs/tcp/package-info.java
    Co-authored-by: François Delbrayelle <fdelbrayelle@gmail.com>

--- src/main/java/io/kestra/plugin/fs/tcp/package-info.java
+++ src/main/java/io/kestra/plugin/fs/tcp/package-info.java
@@ -1,6 +1,6 @@
 @io.kestra.core.models.annotations.PluginSubGroup(
     title = "TCP",
     description = "This sub-group of plugins provides tasks and triggers for interacting with TCP socket connections — including sending and receiving data over TCP.",
-    categories = io.kestra.core.models.annotations.PluginSubGroup.PluginCategory.STORAGE
+    categories = io.kestra.core.models.annotations.PluginSubGroup.PluginCategory.MESSAGING
 )
 package io.kestra.plugin.fs.tcp;
