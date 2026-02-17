ecc28e7d133c9bf0542750028d81f49274147ac8
François Delbrayelle <fdelbrayelle@kestra.io>
2026-02-17 10:03:12 +0100

    Reapply "chore: update plugin categories (#273)"
    This reverts commit 686a50b87061faa33a0100fb4590d6d48e1f82e5.

--- src/main/java/io/kestra/plugin/fs/ftps/package-info.java
+++ src/main/java/io/kestra/plugin/fs/ftps/package-info.java
@@ -1,7 +1,10 @@
 @PluginSubGroup(
     title = "FTPS (File Transfer Protocol Secure)",
     description = "This sub-group of plugins contains tasks for accessing files using the FTPS protocol.",
-    categories = PluginSubGroup.PluginCategory.STORAGE
+    categories = {
+        PluginSubGroup.PluginCategory.DATA,
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.fs.ftps;
 
--- src/main/java/io/kestra/plugin/fs/smb/package-info.java
+++ src/main/java/io/kestra/plugin/fs/smb/package-info.java
@@ -1,7 +1,10 @@
 @PluginSubGroup(
     title = "SMB (Server Message Block)",
     description = "This sub-group of plugins contains tasks for accessing files using the SMB protocol (Samba, for example).",
-    categories = PluginSubGroup.PluginCategory.STORAGE
+    categories = {
+        PluginSubGroup.PluginCategory.DATA,
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.fs.smb;
 
