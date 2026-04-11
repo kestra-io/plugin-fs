b3ec4c29dcbe3b5994dcc5c1df271b497f4ef55d
Malay Dewangan <malaydewangan310@gmail.com>
2026-04-12 01:39:06 +0530

    fix: v2 compatibility

--- src/test/java/io/kestra/plugin/fs/local/AbstractTriggerTest.java
+++ src/test/java/io/kestra/plugin/fs/local/AbstractTriggerTest.java
@@ -3,22 +3,43 @@ package io.kestra.plugin.fs.local;
 import com.devskiller.friendly_id.FriendlyId;
 import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.models.executions.Execution;
-import io.kestra.core.models.property.Property;
+import io.kestra.core.queues.DispatchQueueInterface;
+import io.kestra.core.repositories.LocalFlowRepositoryLoader;
 import io.kestra.core.runners.RunContextFactory;
-import io.kestra.core.utils.TestsUtils;
+import io.kestra.core.runners.Worker;
+import io.kestra.scheduler.AbstractScheduler;
+import io.kestra.core.services.FlowListenersInterface;
+import io.kestra.core.utils.IdUtils;
+import io.kestra.jdbc.runner.JdbcScheduler;
 import io.kestra.plugin.fs.vfs.models.File;
+import io.micronaut.context.ApplicationContext;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Optional;
+import java.util.Objects;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-@KestraTest
+@KestraTest(rebuildContext = true)
 public abstract class AbstractTriggerTest {
+    @Inject
+    private ApplicationContext applicationContext;
+
+    @Inject
+    private FlowListenersInterface flowListenersService;
+
+    @Inject
+    private DispatchQueueInterface<Execution> executionQueue;
+
+    @Inject
+    protected LocalFlowRepositoryLoader repositoryLoader;
+
     @Inject
     protected RunContextFactory runContextFactory;
 
@@ -29,68 +50,99 @@ public abstract class AbstractTriggerTest {
     @Test
     void moveAction() throws Exception {
         String toUploadDir = "/tmp/local-listen";
-        String moveDir = toUploadDir + "-move";
-
         Files.createDirectories(Paths.get(toUploadDir));
 
-        String out1 = FriendlyId.createFriendlyId();
-        utils().upload(toUploadDir + "/" + out1);
+        // mock flow listeners
+        CountDownLatch queueCount = new CountDownLatch(1);
 
-        String out2 = FriendlyId.createFriendlyId();
-        utils().upload(toUploadDir + "/" + out2);
+        // scheduler
+        try (
+            AbstractScheduler scheduler = new JdbcScheduler(
+                this.applicationContext,
+                this.flowListenersService
+            );
+            Worker worker = applicationContext.createBean(Worker.class, IdUtils.create(), 8, null)
+        ) {
+            AtomicReference<Execution> last = new AtomicReference<>();
 
-        var trigger = io.kestra.plugin.fs.local.Trigger.builder()
-            .id(AbstractTriggerTest.class.getSimpleName())
-            .type(io.kestra.plugin.fs.local.Trigger.class.getName())
-            .from(Property.ofValue(toUploadDir))
-            .action(Property.ofValue(Downloads.Action.MOVE))
-            .moveDirectory(Property.ofValue(moveDir))
-            .recursive(Property.ofValue(true))
-            .build();
+            // wait for execution
+            executionQueue.addListener(execution -> {
+                if (execution.getFlowId().equals(triggeringFlowId())) {
+                    last.set(execution);
 
-        var context = TestsUtils.mockTrigger(runContextFactory, trigger);
-        Optional<Execution> execution = trigger.evaluate(context.getKey(), context.getValue());
+                    queueCount.countDown();
+                }
+            });
 
-        assertThat(execution.isPresent(), is(true));
+            String out1 = FriendlyId.createFriendlyId();
+            utils().upload(toUploadDir + "/" + out1);
 
-        @SuppressWarnings("unchecked")
-        java.util.List<File> files = (java.util.List<File>) execution.get().getTrigger().getVariables().get("files");
-        assertThat(files.size(), is(2));
+            String out2 = FriendlyId.createFriendlyId();
+            utils().upload(toUploadDir + "/" + out2);
 
-        assertThat(utils().list(toUploadDir).getFiles().size(), is(0));
+            worker.run();
+            scheduler.run();
+            repositoryLoader.load(Objects.requireNonNull(io.kestra.plugin.fs.local.AbstractTriggerTest.class.getClassLoader().getResource("flows")));
 
-        assertThat(utils().list(moveDir).getFiles().size(), is(2));
+            boolean await = queueCount.await(20, TimeUnit.SECONDS);
+            assertThat(await, is(true));
+
+            @SuppressWarnings("unchecked")
+            java.util.List<File> trigger = (java.util.List<File>) last.get().getTrigger().getVariables().get("files");
+            assertThat(trigger.size(), greaterThanOrEqualTo(2));
+
+            assertThat(utils().list(toUploadDir).getFiles().size(), is(0));
+
+            assertThat(utils().list(toUploadDir + "-move").getFiles().size(), greaterThanOrEqualTo(2));
+            utils().delete(toUploadDir + "-move");
+        }
     }
 
     @Test
     void noneAction() throws Exception {
         String toUploadDir = "/tmp/local-listen-none-action";
-
         Files.createDirectories(Paths.get(toUploadDir));
 
-        String out1 = FriendlyId.createFriendlyId();
-        utils().upload(toUploadDir + "/" + out1);
+        CountDownLatch queueCount = new CountDownLatch(1);
 
-        String out2 = FriendlyId.createFriendlyId();
-        utils().upload(toUploadDir + "/" + out2);
+        // scheduler
+        try (
+            AbstractScheduler scheduler = new JdbcScheduler(
+                this.applicationContext,
+                this.flowListenersService
+            );
+            Worker worker = applicationContext.createBean(Worker.class, IdUtils.create(), 8, null)
+        ) {
+            AtomicReference<Execution> last = new AtomicReference<>();
 
-        var trigger = io.kestra.plugin.fs.local.Trigger.builder()
-            .id(AbstractTriggerTest.class.getSimpleName())
-            .type(io.kestra.plugin.fs.local.Trigger.class.getName())
-            .from(Property.ofValue(toUploadDir))
-            .action(Property.ofValue(Downloads.Action.NONE))
-            .recursive(Property.ofValue(true))
-            .build();
+            // wait for execution
+            executionQueue.addListener(execution -> {
+                if (execution.getFlowId().equals(triggeringFlowId() + "-none-action")) {
+                    last.set(execution);
+                    queueCount.countDown();
+                }
+            });
 
-        var context = TestsUtils.mockTrigger(runContextFactory, trigger);
-        Optional<Execution> execution = trigger.evaluate(context.getKey(), context.getValue());
+            String out1 = FriendlyId.createFriendlyId();
+            utils().upload(toUploadDir + "/" + out1);
 
-        assertThat(execution.isPresent(), is(true));
+            String out2 = FriendlyId.createFriendlyId();
+            utils().upload(toUploadDir + "/" + out2);
 
-        @SuppressWarnings("unchecked")
-        java.util.List<File> files = (java.util.List<File>) execution.get().getTrigger().getVariables().get("files");
-        assertThat(files.size(), is(2));
+            worker.run();
+            scheduler.run();
+            repositoryLoader.load(Objects.requireNonNull(io.kestra.plugin.fs.local.AbstractTriggerTest.class.getClassLoader().getResource("flows")));
 
-        assertThat(utils().list(toUploadDir).getFiles().size(), is(2));
+            boolean await = queueCount.await(20, TimeUnit.SECONDS);
+            assertThat(await, is(true));
+
+            @SuppressWarnings("unchecked")
+            java.util.List<File> trigger = (java.util.List<File>) last.get().getTrigger().getVariables().get("files");
+            assertThat(trigger.size(), greaterThanOrEqualTo(2));
+
+            assertThat(utils().list(toUploadDir).getFiles().size(), greaterThanOrEqualTo(2));
+
+            utils().delete(toUploadDir);
+        }
     }
 }
